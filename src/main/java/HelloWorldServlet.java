0b1ca5113eeb64508cbc7319b8432f3b415dc759
Nia Watson <nia.d.watson@gmail.com>
2022-07-11 10:30:36 -0500

    feat: added Hello World Servlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,18 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, World!</h1>");
     }
+
 }
